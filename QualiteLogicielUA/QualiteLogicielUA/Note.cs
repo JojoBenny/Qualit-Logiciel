ae1d1cc5dd1d390565eba347703bdc2a0cb0e638
NazimTair <162249183+NazimTair@users.noreply.github.com>
2024-04-19 17:50:44 -0700

    Update Note.cs

--- QualiteLogicielUA/QualiteLogicielUA/Note.cs
+++ QualiteLogicielUA/QualiteLogicielUA/Note.cs
@@ -8,5 +8,18 @@ namespace QualiteLogicielUA
 {
     internal class Note
     {
+        // Propriétés de la note
+     public int NumeroEtudiant { get; set; }
+     public int NumeroCours { get; set; }
+     public double NoteValue { get; set; }
+
+     // Constructeur de la classe Note
+     public Note(int numeroEtudiant, int numeroCours, double note)
+     {
+         NumeroEtudiant = numeroEtudiant;
+         NumeroCours = numeroCours;
+         NoteValue = note;
+
+     }
     }
 }
